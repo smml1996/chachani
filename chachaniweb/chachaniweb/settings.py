776e61d7d3738dc0a77a7006ff67a41b2daa954c
smml1996 <michelle_muroya@hotmail.com>
2016-11-27 09:50:59 -0500

    settings

--- chachaniweb/chachaniweb/settings.py
+++ chachaniweb/chachaniweb/settings.py
@@ -122,5 +122,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
+    ##'/var/www/static/',
 ]
