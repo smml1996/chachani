a6f349eab5fcf14c9568d2ee6b3eb4c87e949a58
smml1996 <michelle_muroya@hotmail.com>
2016-12-07 18:42:12 -0500

    fotos

--- chachaniweb/principal/views.py
+++ chachaniweb/principal/views.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
-    template = loader.get_template('base.html')
+    template = loader.get_template('index.html')
     context = {}
     return HttpResponse(template.render(context, request))
