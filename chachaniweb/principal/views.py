ab599290131f343f9b7be48a0dbbabdad06a669d
Paulo Rodriguez <paulo_crr@hotmail.com>
2016-12-29 03:09:59 -0500

    formulario contacto

--- chachaniweb/principal/views.py
+++ chachaniweb/principal/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, Http404
 from django.template import loader
+import json
 
 def index(request):
     template = loader.get_template('index.html')
@@ -18,3 +19,13 @@ def contacto(request):
 def error(request):
     template = loader.get_template('404error.html');
     return HttpResponse(template.render({},request))
+def InsertContacto(request):
+    nombre = request.POST.get("nombre","")
+    response_data={}
+    try:
+        response_data['result']="Exito"
+        response_data['message']=nombre
+    except:
+        response_data['result']="Error"
+        response_data['message']="Error"
+    return HttpResponse(json.dumps(response_data),content_type="application/json")
